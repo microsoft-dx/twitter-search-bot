6f3e7febe861b6a1567ecc218409623e98d88fb9
Radu Matei <matei.radu94@gmail.com>
2016-04-04 20:11:10 +0300

    Added TwitterClient wrapper over Linq2Twitter

---
+++ TwitterBot/TwitterBot/TwitterClient.cs
@@ -0,0 +1,44 @@
+﻿using LinqToTwitter;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace TwitterBot
+{
+    public class TwitterClient
+    {
+        private static TwitterContext TwitterContext { get; set; }
+
+        static TwitterClient()
+        {
+            TwitterContext = new TwitterContext(new SingleUserAuthorizer
+            {
+                CredentialStore = new InMemoryCredentialStore
+                {
+                    ConsumerKey = ConfigurationManager.AppSettings["twitterConsumerKey"],
+                    ConsumerSecret = ConfigurationManager.AppSettings["twitterConsumerSecret"],
+                    OAuthToken = ConfigurationManager.AppSettings["twitterAccessToken"],
+                    OAuthTokenSecret = ConfigurationManager.AppSettings["twitterAccessTokenSecret"]
+                }
+            });
+        }
+
+        public static string GetTweets(string query)
+        {
+            var search = TwitterContext.Search.Where(t => t.Type == SearchType.Search)
+                                  .Where(t => t.Query == query)
+                                  .SingleOrDefault();
+
+            return GetStringTweets(search.Statuses.Take(3));
+        }
+
+        private static string GetStringTweets(IEnumerable<Status> statuses)
+        {
+            string result = "";
+            foreach (var tweet in statuses)
+                result += tweet.ScreenName + "\n\n" + tweet.Text + "\n\n\n";
+
+            return result;
+        }
+    }
+}
