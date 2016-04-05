979a9f95b03bceb3ce327d04e62e70438bdb1f8f
Radu Matei <matei.radu94@gmail.com>
2016-04-05 12:18:46 +0300

    Added LuisResponse

---
+++ TwitterBot/TwitterBot/LuisResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterBot
+{
+    public class LuisResponse
+    {
+        [JsonProperty(PropertyName = "query")]
+        public string Query { get; set; }
+
+        [JsonProperty(PropertyName = "intents")]
+        public Intent[] Intents { get; set; }
+
+        [JsonProperty(PropertyName = "entities")]
+        public Entity[] Entities { get; set; }
+    }
+}
