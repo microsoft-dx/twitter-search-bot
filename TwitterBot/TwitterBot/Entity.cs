d3fa3771e6556d28d08ca464a8bd51b76b49b467
Radu Matei <matei.radu94@gmail.com>
2016-04-05 12:18:25 +0300

    Added LUIS entity

---
+++ TwitterBot/TwitterBot/Entity.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitterBot
+{
+    public class Entity
+    {
+        [JsonProperty(PropertyName = "entity")]
+        public string EntityName { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "startIndex")]
+        public int StartIndex { get; set; }
+
+        [JsonProperty(PropertyName = "endIndex")]
+        public int EndIndex { get; set; }
+
+        [JsonProperty(PropertyName = "score")]
+        public float Score { get; set; }
+    }
+}
