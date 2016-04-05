22cfb09b337ed95c58729b007a5a51daddef8e1f
Radu Matei <matei.radu94@gmail.com>
2016-04-05 12:18:36 +0300

    Added LUIS intent

---
+++ TwitterBot/TwitterBot/Intent.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitterBot
+{
+    public class Intent
+    {
+        [JsonProperty(PropertyName = "intent")]
+        public string IntentName { get; set; }
+
+        [JsonProperty(PropertyName = "score")]
+        public float Score { get; set; }
+
+        [JsonProperty(PropertyName = "actions")]
+        public object Actions { get; set; }
+    }
+}
