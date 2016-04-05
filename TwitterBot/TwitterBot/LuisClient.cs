30b2d619bf675203c92e11efbc867a7884420e44
Radu Matei <matei.radu94@gmail.com>
2016-04-05 12:43:02 +0300

    Added LuisClient

---
+++ TwitterBot/TwitterBot/LuisClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TwitterBot
+{
+    public class LuisClient
+    {
+        private static string url = String.Format("https://api.projectoxford.ai/luis/v1/application?id={0}&subscription-key={1}",
+                            ConfigurationManager.AppSettings["luisApplicationId"], ConfigurationManager.AppSettings["luisSubscriptionKey"]);
+
+
+        public static async Task<LuisResponse> GetLuisResponse(string message)
+        {
+            string query = "&q=" + message;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(url + query);
+                return await response.Content.ReadAsAsync<LuisResponse>();
+            }
+        }
+    }
+}
